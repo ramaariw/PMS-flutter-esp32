613ac2eb89d3cf3050b13dec18bd7dd97281069a
Rama Ari Wahyudi <ramaariwahyudi@gmail.com>
2026-03-13 15:07:32 +0700

    refactor: clean up unused code, imports, and optimize providers for v1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_apps/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'providers/power_provider.dart';
 import 'screens/dashboard.dart';
@@ -21,8 +20,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: DashboardScreen(),
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const DashboardScreen(),
     );
   }
 }
